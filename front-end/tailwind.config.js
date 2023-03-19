6c056b6e5ca9a35a3899cd165b20841297628b05
CJskii <czareczarek3@gmail.com>
2023-03-19 19:17:05 +0000

    Project setup

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        synthwave: {
+          ...require("daisyui/src/colors/themes")["[data-theme=synthwave]"],
+          "base-content": "orange",
+        },
+      },
+    ],
+  },
+};
