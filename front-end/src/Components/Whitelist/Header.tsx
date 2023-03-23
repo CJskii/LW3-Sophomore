61bafdacc93c569f98d42bccdc1dfce77f59b827
CJskii <czareczarek3@gmail.com>
2023-03-23 19:56:42 +0000

    frontend styling

---
+++ front-end/src/Components/Whitelist/Header.tsx
@@ -0,0 +1,9 @@
+const WhitelistHeader = () => {
+  return (
+    <h1 className="text-3xl lg:text-4xl text-blue-200 text-center">
+      Welcome to crypto devs!
+    </h1>
+  );
+};
+
+export default WhitelistHeader;
--- front-end/src/Components/Whitelist/Whitelist.tsx
+++ front-end/src/Components/Whitelist/Whitelist.tsx
@@ -5,6 +5,7 @@ import { getProviderOrSigner } from "../../helpers/providerSigner";
 import { web3ModalContext } from "@/pages/_app";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../../constants/whitelist";
 import { JsonRpcSigner } from "@ethersproject/providers";
+import WhitelistHeader from "./Header";
 
 const Whitelist = () => {
   const [walletConnected, setWalletConnected] = useContext(walletContext);
@@ -12,6 +13,11 @@ const Whitelist = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [numberOfWhitelisted, setNumberOfWhitelisted] = useState<number>(0);
   const [web3modalRef, setWeb3modalRef] = useContext(web3ModalContext);
+
+  useEffect(() => {
+    getNumberOfWhitelisted();
+    checkIfAddressInWhitelist();
+  });
 
   const addAddressToWhitelist = async () => {
     try {
@@ -126,13 +132,25 @@ const Whitelist = () => {
   };
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center grow pt-2 gap-8">
-      <h1 className="text-4xl">Whitelist dApp</h1>
-      <div className="w-full h-full flex flex-col justify-center items-center gap-4">
-        <h1>Welcome to crypto devs!</h1>
-        <div>It's an NFT collection for developers in Crypto.</div>
-        <div>{numberOfWhitelisted} have already joined the Whitelist</div>
-        {renderButton()}
+    <div className="w-full h-full grid grid-rows-4 md:grid-rows-6 max-sm:grid-rows-8 grid-cols-12 grid-flow-rows pt-2 gap-8">
+      <h1 className="text-5xl font-roboto font-bold self-center place-self-center col-start-1 col-span-12 max-lg:row-span-2 max-sm:row-span-1 text-center max-sm:col-start-1 max-sm:text-4xl">
+        Whitelist dApp
+      </h1>
+
+      <div className="row-start-2 row-span-4 col-start-2 xl:col-start-3 col-span-10 xl:col-span-8 max-lg:row-start-3 max-sm:row-start-2 max-lg:col-start-1 max-lg:col-span-12 max-sm:col-span-12 max-[320px]:col-span-11 max-lg:m-2 grid grid-flow-row gap-8 bg-indigo-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100">
+        <div className="w-fit h-full flex flex-col justify-center items-center gap-4 xl:items-start text-yellow-400 self-center place-self-center p-8 max-sm:px-0 text-center">
+          <WhitelistHeader />
+          <div className="text-xl text-blue-200">
+            It's an NFT collection for developers in Crypto.
+          </div>
+          <div className="italic text-lg text-wine max-sm:px-2">
+            {numberOfWhitelisted} user has joined the Whitelist already
+          </div>
+          <div className="text-md text-gray-200">{renderButton()}</div>
+        </div>
+        <div className="p-8 max-lg:w-[300px] max-lg:h-[300px] lg:w-[400px] lg:h-[400px] w-[500px] h-[500px] flex justify-center items-center col-start-2 max-sm:hidden">
+          <img src="./crypto-devs.svg" />
+        </div>
       </div>
     </div>
   );
