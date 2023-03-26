244260e5be661e1bc6bec1862622941d1b92136b
CJskii <czareczarek3@gmail.com>
2023-03-26 20:14:05 +0100

    Render button + data

--- front-end/src/Components/ICO/ICO.tsx
+++ front-end/src/Components/ICO/ICO.tsx
@@ -194,6 +194,36 @@ const ICO = () => {
     }
   };
 
+  // ___ CLAIM TOKENS ___
+  const claimTokens = async () => {
+    try {
+      // initiate signer instance - we will be writing to the contract
+      const signer = await getProviderOrSigner({
+        needSigner: true,
+        web3modalRef,
+      });
+
+      // create token contract instance
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+
+      const tx = await tokenContract.claim();
+      setLoading(true);
+      // wait for the transaction to get mined
+      await tx.wait();
+      setLoading(false);
+      window.alert("Sucessfully claimed Crypto Dev Tokens");
+      await getBalanceOfTokens();
+      await getTotalTokensMinted();
+      await getTokensToBeClaimed();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   // ___ CHECK HOW MANY TOKENS HAVE BEEN MINTED OUT OF TOTAL SUPPLY ___
 
   const getTotalTokensMinted = async () => {
@@ -285,9 +315,76 @@ const ICO = () => {
     }
   };
 
+  // returns a button based on the state of app
+
+  const renderButton = () => {
+    // If we are currently waiting for something, return a loading button
+    if (loading) {
+      return (
+        <div>
+          <button className="btn loading">Loading...</button>
+        </div>
+      );
+    }
+    // If tokens to be claimed are greater than 0, Return a claim button
+    if (tokensToBeClaimed > 0) {
+      return (
+        <div>
+          <div className="">
+            {tokensToBeClaimed * 10} Tokens can be claimed!
+          </div>
+          <button className="btn" onClick={claimTokens}>
+            Claim Tokens
+          </button>
+        </div>
+      );
+    }
+    // If user doesn't have any tokens to claim, show the mint button
+    return (
+      <div style={{ display: "flex-col" }}>
+        <div>
+          <input
+            type="number"
+            placeholder="Amount of Tokens"
+            // BigNumber.from converts the `e.target.value` to a BigNumber
+            onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))}
+            className=""
+          />
+        </div>
+
+        <button
+          className=""
+          disabled={!(tokenAmount > 0)}
+          onClick={() => mintTokens(tokenAmount)}
+        >
+          Mint Tokens
+        </button>
+      </div>
+    );
+  };
+
   return (
-    <div className="w-full h-full flex justify-center items-center grow pt-8">
-      <h1 className="text-5xl">ICO app</h1>
+    <div className="w-full h-full grid grid-rows-4 md:grid-rows-6 max-sm:grid-rows-8 grid-cols-12 grid-flow-rows pt-2 gap-8">
+      <h1 className="text-5xl font-montserrat font-bold self-center place-self-center col-start-1 col-span-12 max-lg:row-span-2 max-sm:row-span-1 text-center max-sm:col-start-1 max-sm:text-4xl">
+        ICO dApp
+      </h1>
+      <div className="row-start-2 row-span-4 col-start-2 xl:col-start-3 col-span-10 xl:col-span-8 max-lg:row-start-3 max-sm:row-start-2 max-lg:col-start-1 max-lg:col-span-12 max-sm:col-span-12 max-[320px]:col-span-11 max-lg:m-2 grid grid-flow-row gap-8 bg-indigo-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100">
+        <div className="w-fit h-full font-roboto flex flex-col justify-center items-center gap-4 xl:items-start text-yellow-400 self-center place-self-center p-8 max-sm:px-0 text-center">
+          <h1 className="text-3xl lg:text-4xl text-blue-200 text-center">
+            Welcome to Crypto Devs!
+          </h1>
+          <div className="text-xl text-blue-200">
+            It&#39;s an NFT collection for developers in Crypto.
+          </div>
+          <div className="italic text-lg text-wine max-sm:px-2">
+            {tokenIdsMinted}/20 have been minted
+          </div>
+          <div className="text-md text-gray-200">{renderButton()}</div>
+        </div>
+        <div className="p-8 max-lg:w-[300px] max-lg:h-[300px] lg:w-[400px] lg:h-[400px] w-[500px] h-[500px] flex justify-center items-center col-start-2 max-sm:hidden">
+          {/* image */}
+        </div>
+      </div>
     </div>
   );
 };
