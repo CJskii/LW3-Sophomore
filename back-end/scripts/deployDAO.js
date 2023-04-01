26f591eb118fd18ba6b3ce3eb2a89d5a8684292d
CJskii <czareczarek3@gmail.com>
2023-04-01 18:03:19 +0100

    Marketplace + DAO deploy scripts

---
+++ back-end/scripts/deployDAO.js
@@ -0,0 +1,30 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { FAKE_NFT_MARKETPLACE_CONTRACT_ADDRESS } = require("../constants/index");
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants/index");
+
+async function main() {
+  // Deploy the CryptoDevsDAO contract
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    FAKE_NFT_MARKETPLACE_CONTRACT_ADDRESS,
+    CRYPTO_DEVS_NFT_CONTRACT_ADDRESS,
+    {
+      // This assumes your metamask account has at least 1 ETH in its account
+      // Change this value as you want
+      value: ethers.utils.parseEther("1"),
+    }
+  );
+  console.log("deploying CryptoDevsDAO contract...");
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ back-end/scripts/deployFakeMarketplace.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
+  );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
