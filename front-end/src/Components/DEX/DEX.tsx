bde6191002a7e8fba05534ff6e75d471acb84f58
CJskii <czareczarek3@gmail.com>
2023-04-07 23:41:27 +0100

    Functional swap component logic

--- front-end/src/Components/DEX/DEX.tsx
+++ front-end/src/Components/DEX/DEX.tsx
@@ -1,27 +1,16 @@
 import React, { useEffect, useContext, useState } from "react";
 import { walletContext } from "@/pages/_app";
 import { web3ModalContext } from "@/pages/_app";
-import getTokenContractInstance from "@/utils/contract/getTokencontract";
-import getExchangeContractInstance from "@/utils/contract/getExchangecontract";
 import currentAddress from "@/utils/getAddress";
 import { getProviderOrSigner } from "@/utils/providerSigner";
-import { BigNumber, utils } from "ethers";
+import { BigNumber } from "ethers";
 import Web3Modal from "web3modal";
-import { addLiquidity, calculateCD } from "@/utils/Exchange/addLiquidity";
 import {
   getCDTokensBalance,
   getEtherBalance,
   getLPTokensBalance,
   getReserveOfCDTokens,
 } from "@/utils/Exchange/getAmounts";
-import {
-  getTokensAfterRemove,
-  removeLiquidity,
-} from "@/utils/Exchange/removeLiquidity";
-import {
-  swapTokens,
-  getAmountOfTokensReceivedFromSwap,
-} from "@/utils/Exchange/swap";
 import Liquidity from "./Liquidity";
 import Swap from "./Swap";
 import OverviewDEX from "./OverviewDEX";
@@ -46,28 +35,6 @@ const DEX = () => {
   const [cdBalance, setCDBalance] = useState(zero);
   // lpBalance is the amount of LP tokens in the user's account
   const [lpBalance, setLPBalance] = useState(zero);
-
-  // ___ VARIBALES FOR KEEPING TRACK OF LIQUIDITY ___
-
-  const [addEther, setAddEther] = useState(zero);
-  // addCDTokens keeps track of the amount of CD tokens to be added to the liquidity pool
-  const [addCDTokens, setAddCDTokens] = useState(zero);
-  // removeEther is the amount of ether that would be send back to user based on the amount of LP tokens he/she wants to remove
-  const [removeEther, setRemoveEther] = useState(zero);
-  // removeCD is the amount of tokens that would be send back to user based on the amount of LP tokens he/she wants to remove
-  const [removeCD, setRemoveCD] = useState(zero);
-  // amount of LP tokens that user wants to remove
-  const [removeLPTokens, setRemoveLPTokens] = useState("0");
-
-  // ___ VARIABLES FOR KEEPING TRACK OF SWAP ___
-
-  // amount user wants to swap
-  const [swapAmount, setSwapAmount] = useState("");
-  // This keeps track of the number of tokens that the user would receive after the swap
-  const [tokenToBeReceivedAfterSwap, setTokenToBeReceivedAfterSwap] =
-    useState(zero);
-  // keeps track of whether eth or cd is selected for swap
-  const [ethSelected, setEthSelected] = useState(true);
 
   // ___ CONNECT WALLET ___
 
@@ -122,153 +89,6 @@ const DEX = () => {
     setEtherBalanceContract(_ethBalanceContract);
   };
 
-  // ___ SWAP FUNCTIONS ___
-
-  // Swaps 'swapAmountWei' of Eth/CD tokens with 'tokenToBeReceivedAfterSwap' amount of CD/Eth tokens
-
-  const _swapTokens = async () => {
-    try {
-      // convert the amount entered by user to BigNumber using parseEther
-      const swapAmountWei = utils.parseEther(swapAmount);
-      // check if user entered 0
-      if (!swapAmountWei.eq(zero)) {
-        const signer = await getProviderOrSigner({
-          needSigner: true,
-          web3modalRef,
-        });
-        setLoading(true);
-        // call the swaptokens function from utils
-        await swapTokens(
-          signer,
-          swapAmountWei,
-          tokenToBeReceivedAfterSwap,
-          ethSelected
-        );
-        setLoading(false);
-        // update the balances
-        await getAmounts();
-        setSwapAmount("");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // get the amount of tokens that the user would receive after the swap
-  const _getAmountOfTokensReceivedFromSwap = async (_swapAmount: number) => {
-    try {
-      // convert the amount entered by user to BigNumber using parseEther
-      const _swapAmountWEI = utils.parseEther(_swapAmount.toString());
-      // check if user entered 0
-      if (!_swapAmountWEI.eq(zero)) {
-        const provider = await getProviderOrSigner({
-          needSigner: false,
-          web3modalRef,
-        });
-        const _ethBalance = await getEtherBalance(provider, null, true);
-        // call the getAmountOfTokensReceivedFromSwap function from utils
-        const amountOfTokens = await getAmountOfTokensReceivedFromSwap(
-          _swapAmountWEI,
-          provider,
-          ethSelected,
-          _ethBalance,
-          reservedCD
-        );
-        setTokenToBeReceivedAfterSwap(amountOfTokens);
-      } else {
-        setTokenToBeReceivedAfterSwap(zero);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // ___ ADD LIQUIDITY FUNCTIONS ___
-
-  /**
-   * _addLiquidity helps add liquidity to the exchange,
-   * If the user is adding initial liquidity, user decides the ether and CD tokens he wants to add
-   * to the exchange. If he is adding the liquidity after the initial liquidity has already been added
-   * then we calculate the crypto dev tokens he can add, given the Eth he wants to add by keeping the ratios
-   * constant
-   */
-
-  const _addLiquidity = async () => {
-    try {
-      // convert the amount entered by user to BigNumber using parseEther
-      const addEtherWei = utils.parseEther(addEther.toString());
-      // check if user entered 0
-      if (!addEtherWei.eq(zero)) {
-        const signer = await getProviderOrSigner({
-          needSigner: true,
-          web3modalRef,
-        });
-        setLoading(true);
-        // call the addLiquidity function from utils
-        await addLiquidity(signer, addEtherWei, addCDTokens);
-        setLoading(false);
-        // reinitialize CD tokens
-        setAddCDTokens(zero);
-        // update the balances
-        await getAmounts();
-      }
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
-  // ___ REMOVE LIQUIDITY FUNCTIONS ___
-
-  /**
-   * _removeLiquidity: Removes the `removeLPTokensWei` amount of LP tokens from
-   * liquidity and also the calculated amount of `ether` and `CD` tokens
-   */
-
-  const _removeLiquidity = async () => {
-    try {
-      const signer = await getProviderOrSigner({
-        needSigner: true,
-        web3modalRef,
-      });
-      setLoading(true);
-      // convert the amount entered by user to BigNumber using parseEther
-      const removeLPTokensWei = utils.parseEther(removeLPTokens.toString());
-      await removeLiquidity(signer, removeLPTokensWei);
-      setLoading(false);
-      // update the balances
-      await getAmounts();
-      setRemoveCD(zero);
-      setRemoveEther(zero);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
-  const _getTokensAfterRemove = async () => {
-    try {
-      const provider = await getProviderOrSigner({
-        needSigner: false,
-        web3modalRef,
-      });
-      const removeLPTokensWei = utils.parseEther(removeLPTokens);
-      const _ethBalance = await getEtherBalance(provider, null, true);
-      const cryptoDevTokenReserve = await getReserveOfCDTokens(provider);
-
-      const { _removeEther, _removeCD }: any = await getTokensAfterRemove(
-        provider,
-        removeLPTokensWei,
-        _ethBalance,
-        cryptoDevTokenReserve
-      );
-      setRemoveEther(_removeEther);
-      setRemoveCD(_removeCD);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const renderButton = () => {
     if (!walletConnected) {
       return (
@@ -288,12 +108,23 @@ const DEX = () => {
       const liquidityProps = {
         // enter props here
         setSelectedTab,
+        web3modalRef,
+        getAmounts,
+        ethBalance,
+        cdBalance,
+        lpBalance,
+        reservedCD,
+        etherBalanceContract,
+        setEtherBalanceContract,
       };
       return <Liquidity {...liquidityProps} />;
     } else if (selectedTab == "Swap") {
       const swapProps = {
         // enter props here
         setSelectedTab,
+        web3modalRef,
+        getAmounts,
+        reservedCD,
       };
       return <Swap {...swapProps} />;
     } else {
--- front-end/src/Components/DEX/Swap.tsx
+++ front-end/src/Components/DEX/Swap.tsx
@@ -1,14 +1,103 @@
-import { utils } from "ethers";
+import { utils, BigNumber } from "ethers";
+import React, { useState } from "react";
+import { getProviderOrSigner } from "@/utils/providerSigner";
+import { getEtherBalance } from "@/utils/Exchange/getAmounts";
+import {
+  swapTokens,
+  getAmountOfTokensReceivedFromSwap,
+} from "@/utils/Exchange/swap";
 
 interface Props {
   setSelectedTab: (tab: string) => void;
+  web3modalRef: any;
+  getAmounts: () => void;
+  reservedCD: BigNumber;
 }
 
 const Swap = (props: Props) => {
-  const { setSelectedTab } = props;
+  const { setSelectedTab, web3modalRef, getAmounts, reservedCD } = props;
+
+  const zero = BigNumber.from(0);
+  const [loading, setLoading] = useState(false);
+
+  // ___ VARIABLES FOR KEEPING TRACK OF SWAP ___
+
+  // amount user wants to swap
+  const [swapAmount, setSwapAmount] = useState("");
+  // This keeps track of the number of tokens that the user would receive after the swap
+  const [tokenToBeReceivedAfterSwap, setTokenToBeReceivedAfterSwap] =
+    useState(zero);
+  // keeps track of whether eth or cd is selected for swap
+  const [ethSelected, setEthSelected] = useState(true);
+
+  // ___ SWAP FUNCTIONS ___
+
+  // Swaps 'swapAmountWei' of Eth/CD tokens with 'tokenToBeReceivedAfterSwap' amount of CD/Eth tokens
+
+  const _swapTokens = async () => {
+    try {
+      // convert the amount entered by user to BigNumber using parseEther
+      const swapAmountWei = utils.parseEther(swapAmount);
+      // check if user entered 0
+      if (!swapAmountWei.eq(zero)) {
+        const signer = await getProviderOrSigner({
+          needSigner: true,
+          web3modalRef,
+        });
+        setLoading(true);
+        // call the swaptokens function from utils
+        await swapTokens(
+          signer,
+          swapAmountWei,
+          tokenToBeReceivedAfterSwap,
+          ethSelected
+        );
+        setLoading(false);
+        // update the balances
+        getAmounts();
+        setSwapAmount("");
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  // get the amount of tokens that the user would receive after the swap
+  const _getAmountOfTokensReceivedFromSwap = async (
+    _swapAmount: number | string
+  ) => {
+    try {
+      // convert the amount entered by user to BigNumber using parseEther
+      const _swapAmountWEI = utils.parseEther(_swapAmount.toString());
+      // check if user entered 0
+      if (!_swapAmountWEI.eq(zero)) {
+        const provider = await getProviderOrSigner({
+          needSigner: false,
+          web3modalRef,
+        });
+        const _ethBalance = await getEtherBalance(provider, null, true);
+        // call the getAmountOfTokensReceivedFromSwap function from utils
+        const amountOfTokens = await getAmountOfTokensReceivedFromSwap(
+          _swapAmountWEI,
+          provider,
+          ethSelected,
+          _ethBalance,
+          reservedCD
+        );
+        setTokenToBeReceivedAfterSwap(amountOfTokens);
+      } else {
+        setTokenToBeReceivedAfterSwap(zero);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
   return (
-    <>
-      <div className="tabs">
+    <div className="flex flex-col justify-start items-center w-full">
+      <div className="tabs w-full">
         <a
           className="tab tab-lifted text-slate-400 hover:text-slate-200"
           onClick={() => setSelectedTab("")}
@@ -23,46 +112,52 @@ const Swap = (props: Props) => {
           Add liquidity
         </a>
       </div>
-    </>
-    // <div>
-    //   <input
-    //     type="number"
-    //     placeholder="Amount"
-    //     onChange={async (e) => {
-    //       setSwapAmount(e.target.value || "");
-    //       // Calculate the amount of tokens user would receive after the swap
-    //       await _getAmountOfTokensReceivedFromSwap(e.target.value || "0");
-    //     }}
-    //     className=""
-    //     value={swapAmount}
-    //   />
-    //   <select
-    //     className=""
-    //     name="dropdown"
-    //     id="dropdown"
-    //     onChange={async () => {
-    //       setEthSelected(!ethSelected);
-    //       // Initialize the values back to zero
-    //       await _getAmountOfTokensReceivedFromSwap(0);
-    //       setSwapAmount("");
-    //     }}
-    //   >
-    //     <option value="eth">Ethereum</option>
-    //     <option value="cryptoDevToken">Crypto Dev Token</option>
-    //   </select>
-    //   <br />
-    //   <div className="">
-    //     {/* Convert the BigNumber to string using the formatEther function from ethers.js */}
-    //     {ethSelected
-    //       ? `You will get ${utils.formatEther(
-    //           tokenToBeReceivedAfterSwap
-    //         )} Crypto Dev Tokens`
-    //       : `You will get ${utils.formatEther(tokenToBeReceivedAfterSwap)} Eth`}
-    //   </div>
-    //   <button className="btn" onClick={_swapTokens}>
-    //     Swap
-    //   </button>
-    // </div>
+      <div className="flex flex-col justify-center items-center gap-4 mt-8 p-8 rounded min-w-[300px] bg-slate-800">
+        <input
+          type="number"
+          placeholder="Amount"
+          onChange={async (e) => {
+            setSwapAmount(e.target.value || "");
+            // Calculate the amount of tokens user would receive after the swap
+            await _getAmountOfTokensReceivedFromSwap(e.target.value || "0");
+          }}
+          className="text-white p-[5px] text-md"
+          value={swapAmount}
+        />
+        <select
+          className="text-slate-300 p-[5px] text-md"
+          name="dropdown"
+          id="dropdown"
+          onChange={async () => {
+            setEthSelected(!ethSelected);
+            // Initialize the values back to zero
+            await _getAmountOfTokensReceivedFromSwap(0);
+            setSwapAmount("");
+          }}
+        >
+          <option value="eth">Ethereum</option>
+          <option value="cryptoDevToken">Crypto Dev Token</option>
+        </select>
+        <br />
+        <div className="text-blue-200 text-xl">
+          {/* Convert the BigNumber to string using the formatEther function from ethers.js */}
+          {ethSelected
+            ? `You will get ${parseFloat(
+                utils.formatEther(tokenToBeReceivedAfterSwap)
+              ).toFixed(2)} Crypto Dev Tokens`
+            : `You will get ${parseFloat(
+                utils.formatEther(tokenToBeReceivedAfterSwap)
+              ).toFixed(2)} ETH`}
+        </div>
+        {loading ? (
+          <button className="btn loading">Loading</button>
+        ) : (
+          <button className="btn" onClick={_swapTokens}>
+            Swap
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
